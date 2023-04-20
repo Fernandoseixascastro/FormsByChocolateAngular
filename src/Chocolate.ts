cab23600a6035c31c246c0802e3bd9dfdc1da922
Fernando Luiz <fernandoluizseixascastro@hotmail.com>
2023-04-20 18:17:25 -0300

    Uploading

---
+++ src/Chocolate.ts
@@ -0,0 +1,6 @@
+export class Chocolate{
+    
+    public nome: string | undefined;
+    public marca: string | undefined;   
+    public preco: number | undefined;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Chocolate } from 'src/Chocolate';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularPweb';
+
+  chocolate : Chocolate;
+  chocolates: Array<Chocolate>;
+
+  constructor(){
+    this.chocolate = new Chocolate();
+    this.chocolates = new Array<Chocolate>();
+    
+  }
+
+  inserirChocolate(){
+    this.chocolates.push(this.chocolate);
+    this.chocolate = new Chocolate();
+  }
+
+  removerChocolate(nomeChocolate: Chocolate){
+    const index = this.chocolates.findIndex(
+      chocolate => chocolate === nomeChocolate
+    );
+    this.chocolates.splice(index, 1);
+  }
+
+  // editarChocolate(choco: Chocolate): void {
+  //   const index = this.chocolates.findIndex(
+  //     chocolate => chocolate === this.chocolate
+  //   );
+  //   this.chocolates[index] = this.chocolate;
+  //   this.chocolate = new Chocolate();
+  // }
+
+  editarChocolate(chocolateEditado: Chocolate){
+    this.chocolate = chocolateEditado;
+  }
+
+
+  // editarChocolate(choco: Chocolate): void {
+  //   const index = this.chocolates.findIndex(p => p.nome === choco.nome);
+  //   this.chocolates[index] = choco;
+  // }
+
+  listarChocolates(){
+    return this.chocolates;
+  }
 }
